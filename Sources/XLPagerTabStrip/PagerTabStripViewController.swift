275e56dc6a1cf4c8be48e46430c281b099cfd01c
Ivan Jovanovik <Ivan.jovanovik@vpar.com>
2024-03-26 09:04:44 +0100

    another toast logging

--- Sources/XLPagerTabStrip/PagerTabStripViewController.swift
+++ Sources/XLPagerTabStrip/PagerTabStripViewController.swift
@@ -154,6 +154,22 @@ open class PagerTabStripViewController: UIViewController, UIScrollViewDelegate {
             preCurrentIndex = index
             return
         }
+        
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = .systemFont(ofSize: 12.0)
+        toastLabel.textAlignment = .center;
+        toastLabel.text = "\(self.viewControllers[currentIndex].tabBarItem.tag)"
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 2.0, delay: 0.1, options: .curveEaseOut, animations: {
+             toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
 
         if animated && pagerBehaviour.skipIntermediateViewControllers && abs(currentIndex - index) > 1 {
             var tmpViewControllers = viewControllers
