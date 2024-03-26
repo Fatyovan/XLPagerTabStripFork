8b1e837056ec4cfcd824955fab026bc5704b4896
Ivan Jovanovik <Ivan.jovanovik@vpar.com>
2024-03-26 09:18:28 +0100

    revert indicatorInfo and toast

--- Sources/XLPagerTabStrip/IndicatorInfo.swift
+++ Sources/XLPagerTabStrip/IndicatorInfo.swift
@@ -34,7 +34,7 @@ public struct IndicatorInfo {
     public var userInfo: Any?
     
     public init(title: String?) {
-        self.title = "Ivan"
+        self.title = title
         self.accessibilityLabel = title
     }
     
